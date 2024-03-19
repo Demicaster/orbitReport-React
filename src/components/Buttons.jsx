918f723ba14f973574c750a2ee8336c2e02c5e6e
Joseph <Demicaster@gmail.com>
2024-03-18 19:02:26 -0500

    Finished Graded assignment 6

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,23 @@
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
-};
+  
+  let buttons = displaySats.map((sat, id) => {
+    return (
+   
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+   
+    );
+  })
+  let secondButton = <button onClick={() => {setSat(satData)}}>
+    All Orbits
+  </button>
+  buttons.push(secondButton);
+  return  (   <div>{buttons}</div>) ;
+  
+}
+
 
 export default Buttons;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,37 @@
 const Table = ({ sat }) => {
+
+ 
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((data, id) =>{
+      let statusCheck;
+      if(data.operational == true){
+        statusCheck = "satellite is active."
+
+      }
+      else{
+        statusCheck = "satellite  is inactive "
+      }
+    return(
+      <tr key={id}>
+    <th>{data.name}</th>
+    <th>{data.type}</th>
+    <th>{data.launchDate}</th>
+    <th>{data.orbitType}</th>
+    <th>{statusCheck}</th>
+    
+    </tr>
+  )
+  })}
      </tbody>
    </table>
   );
